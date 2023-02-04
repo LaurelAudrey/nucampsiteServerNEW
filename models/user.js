f9280d6db11f9022fa7268b57bef5e7ea33e3335
Laurel Elm <laurel.elm@gmail.com>
2023-02-04 05:42:32 -0700

    Add FB passport module to authenticate.js

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
         type: String,
         default: ''
     },
-    facebookID: String,
+    facebookId: String,
     admin: {
         type: Boolean,
         default: false
